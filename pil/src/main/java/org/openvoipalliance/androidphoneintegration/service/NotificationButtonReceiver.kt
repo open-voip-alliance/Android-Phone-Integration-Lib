32fb8f81337552c2decdf769faddb9692e229e0d
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-09-09 15:54:29 +0100

    Cancel incoming call notification on button press when no active call

--- pil/src/main/java/org/openvoipalliance/androidphoneintegration/service/NotificationButtonReceiver.kt
+++ pil/src/main/java/org/openvoipalliance/androidphoneintegration/service/NotificationButtonReceiver.kt
@@ -4,13 +4,33 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import org.openvoipalliance.androidphoneintegration.PIL
+import org.openvoipalliance.androidphoneintegration.helpers.stopVoipService
+import org.openvoipalliance.androidphoneintegration.notifications.IncomingCallNotification
 import org.openvoipalliance.androidphoneintegration.service.NotificationButtonReceiver.Action.*
 
 internal class NotificationButtonReceiver : BroadcastReceiver() {
 
     private val pil by lazy { PIL.instance }
+    private val incomingCallNotification by lazy { IncomingCallNotification() }
 
     override fun onReceive(context: Context, intent: Intent) {
+        // If there is no PIL, we should no longer be responding to the notifications being
+        // pressed.
+        if (!PIL.isInitialized) {
+            incomingCallNotification.cancel()
+            context.stopVoipService()
+            return
+        }
+
+        // This exists to resolve a problem where the incoming call notification would remain
+        // visible even when there is no call. A solution should be found to fix the underlying
+        // issue but currently this at least provides a way out for users so their phone
+        // stops ringing.
+        if (!pil.calls.isInCall) {
+            incomingCallNotification.cancel()
+            return
+        }
+
         try {
             val action = valueOf(intent.action ?: "")
 
