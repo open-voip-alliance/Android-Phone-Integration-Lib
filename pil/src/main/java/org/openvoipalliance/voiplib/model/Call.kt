e24dcc89d6fb8eb7f201be5ac4f8f23942685881
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-12-11 10:30:10 +0000

    Added callId and reason to call object

--- pil/src/main/java/org/openvoipalliance/voiplib/model/Call.kt
+++ pil/src/main/java/org/openvoipalliance/voiplib/model/Call.kt
@@ -43,30 +43,9 @@ class Call(val linphoneCall: LinphoneCall) {
         }
 
     val reason
-        get() = when (linphoneCall.reason) {
-                Reason.None -> org.openvoipalliance.voiplib.model.Reason.NONE
-                Reason.NoResponse -> org.openvoipalliance.voiplib.model.Reason.NO_RESPONSE
-                Reason.Declined -> org.openvoipalliance.voiplib.model.Reason.DECLINED
-                Reason.NotFound -> org.openvoipalliance.voiplib.model.Reason.NOT_FOUND
-                Reason.NotAnswered -> org.openvoipalliance.voiplib.model.Reason.NOT_ANSWERED
-                Reason.Busy -> org.openvoipalliance.voiplib.model.Reason.BUSY
-                Reason.IOError -> org.openvoipalliance.voiplib.model.Reason.IO_ERROR
-                Reason.DoNotDisturb -> org.openvoipalliance.voiplib.model.Reason.DO_NOT_DISTURB
-                Reason.Unauthorized -> org.openvoipalliance.voiplib.model.Reason.UNAUTHORISED
-                Reason.NotAcceptable -> org.openvoipalliance.voiplib.model.Reason.NOT_ACCEPTABLE
-                Reason.NoMatch -> org.openvoipalliance.voiplib.model.Reason.NO_MATCH
-                Reason.MovedPermanently -> org.openvoipalliance.voiplib.model.Reason.MOVED_PERMANENTLY
-                Reason.Gone -> org.openvoipalliance.voiplib.model.Reason.GONE
-                Reason.TemporarilyUnavailable -> org.openvoipalliance.voiplib.model.Reason.TEMPORARILY_UNAVAILABLE
-                Reason.AddressIncomplete -> org.openvoipalliance.voiplib.model.Reason.ADDRESS_INCOMPLETE
-                Reason.NotImplemented -> org.openvoipalliance.voiplib.model.Reason.NOT_IMPLEMENTED
-                Reason.BadGateway -> org.openvoipalliance.voiplib.model.Reason.BAD_GATEWAY
-                Reason.ServerTimeout -> org.openvoipalliance.voiplib.model.Reason.SERVER_TIMEOUT
-                Reason.Unknown -> org.openvoipalliance.voiplib.model.Reason.UNKNOWN
-                else -> org.openvoipalliance.voiplib.model.Reason.UNKNOWN
-            }
+        get() = linphoneCall.reason.name
 
-    val callId: String?
+    val callId: String
         get() = linphoneCall.callLog.callId
 
     val direction = when (linphoneCall.callLog.dir) {
