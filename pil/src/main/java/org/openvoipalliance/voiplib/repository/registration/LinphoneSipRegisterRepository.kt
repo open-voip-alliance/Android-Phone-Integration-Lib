fb19d20753f16afc6fd2d93ba7bb7fbde25633e9
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-12-14 13:28:00 +0100

    Allowing for registration to fail and then succeed.

--- pil/src/main/java/org/openvoipalliance/voiplib/repository/registration/LinphoneSipRegisterRepository.kt
+++ pil/src/main/java/org/openvoipalliance/voiplib/repository/registration/LinphoneSipRegisterRepository.kt
@@ -1,12 +1,14 @@
 package org.openvoipalliance.voiplib.repository.registration
 
-import android.util.Log
 import org.linphone.core.*
+import org.openvoipalliance.androidphoneintegration.log
 import org.openvoipalliance.voiplib.RegistrationCallback
 import org.openvoipalliance.voiplib.model.RegistrationState.FAILED
 import org.openvoipalliance.voiplib.model.RegistrationState.REGISTERED
 import org.openvoipalliance.voiplib.repository.LinphoneCoreInstanceManager
 import org.openvoipalliance.voiplib.repository.SimpleCoreListener
+import java.util.*
+import kotlin.concurrent.schedule
 
 internal class LinphoneSipRegisterRepository(private val linphoneCoreInstanceManager: LinphoneCoreInstanceManager) {
 
@@ -29,12 +31,12 @@ internal class LinphoneSipRegisterRepository(private val linphoneCoreInstanceMan
         this.callback = callback
 
         if (core.proxyConfigList.isNotEmpty()) {
-            linphoneCoreInstanceManager.log("Proxy config found, re-registering.")
+            registerLog("Proxy config found, re-registering.")
             core.refreshRegisters()
             return
         }
 
-        linphoneCoreInstanceManager.log("No proxy config found, registering for the first time.")
+        registerLog("No proxy config found, registering for the first time.")
 
         if (config.auth.port < 1 || config.auth.port > 65535) {
             throw IllegalArgumentException("Unable to register with a server when port is invalid: ${config.auth.port}")
@@ -95,25 +97,101 @@ internal class LinphoneSipRegisterRepository(private val linphoneCoreInstanceMan
     fun isRegistered() = linphoneCoreInstanceManager.state.isRegistered
 
     private inner class RegistrationListener : SimpleCoreListener {
+
+        /**
+         * It is sometimes possible that a failed registration will occur before a successful one
+         * so we will track the time of the first registration update before determining it has
+         * failed.
+         */
+        private var startTime: Long? = null
+
+        private val currentTime: Long
+            get() = System.currentTimeMillis()
+
+        private var timer: Timer? = null
+            set(timer) {
+                timer?.cancel()
+                timer?.purge()
+                field = timer
+            }
+
         override fun onAccountRegistrationStateChanged(
             core: Core,
             account: Account,
             state: RegistrationState?,
-            message: String
+            message: String,
         ) {
-            linphoneCoreInstanceManager.log("Registration state change: ${state?.name} - $message")
+            registerLog("State change: ${state?.name} - $message")
 
-            if (state == RegistrationState.Failed || state == RegistrationState.Ok) {
-                linphoneCoreInstanceManager.state.isRegistered = state == RegistrationState.Ok
-                callback?.invoke(if (state == RegistrationState.Ok) REGISTERED else FAILED)
-                callback = null
+            val callback = this@LinphoneSipRegisterRepository.callback ?: run {
+                registerLog("Callback set so registration state change has not done anything.")
+                reset()
+                return
+            }
 
-                // If the registration state has failed, we want to remove the proxy config
-                // so it is possible to retry in the future.
-                if (state == RegistrationState.Failed) {
-                    unregister()
+            // If the registration was successful, just immediately invoke the callback and reset
+            // all timers.
+            if (state == RegistrationState.Ok) {
+                registerLog("Successful, resetting timers.")
+                callback.invoke(REGISTERED)
+                reset()
+                return
+            }
+
+            // If there is no start time, we want to set it to begin the timer.
+            val startTime = this.startTime ?: run {
+                val startTime = currentTime
+                this.startTime = startTime
+                registerLog("Started registration timer: $startTime.")
+                startTime
+            }
+
+            if (hasExceededTimeout(startTime)) {
+                unregister()
+                registerLog("Registration timeout has been exceeded, registration failed.")
+                callback.invoke(FAILED)
+                reset()
+                return
+            }
+
+            // Queuing call of this method so we ensure that the callback is eventually invoked
+            // even if there are no future registration updates.
+            timer = Timer("Registration").also {
+                it.schedule(cleanUpDelay) {
+                    onAccountRegistrationStateChanged(
+                        core,
+                        account,
+                        state,
+                        "Automatically called to ensure callback is executed"
+                    )
                 }
             }
         }
+
+        private fun hasExceededTimeout(startTime: Long): Boolean =
+            (startTime + registrationTimeoutMs) < currentTime
+
+        private fun reset() {
+            this@LinphoneSipRegisterRepository.callback = null
+            startTime = null
+            timer?.apply {
+                cancel()
+                purge()
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * The amount of time to wait before determining registration has failed.
+         */
+        const val registrationTimeoutMs = 5000L
+
+        /**
+         * The time that we will wait before executing the method again to clean-up.
+         */
+        const val cleanUpDelay = 1000L
     }
 }
+
+private fun registerLog(message: String) = log("SIP-REGISTER: $message")
