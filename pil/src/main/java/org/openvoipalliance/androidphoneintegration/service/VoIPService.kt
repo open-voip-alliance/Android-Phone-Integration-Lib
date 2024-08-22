1539324ec27f62c15a13b95dbc19524dd2e8f8eb
Jeremy Norman <jeremynorman89@gmail.com>
2024-08-22 17:33:43 +0200

    chore: no longer using foreground service microphone

--- pil/src/main/java/org/openvoipalliance/androidphoneintegration/service/VoIPService.kt
+++ pil/src/main/java/org/openvoipalliance/androidphoneintegration/service/VoIPService.kt
@@ -56,13 +56,7 @@ internal class VoIPService : CoreService(), PILEventListener {
 
     private fun showForegroundNotification(notification: Notification, id: Int) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val types = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                FOREGROUND_SERVICE_TYPE_MICROPHONE or FOREGROUND_SERVICE_TYPE_PHONE_CALL
-            } else {
-                FOREGROUND_SERVICE_TYPE_PHONE_CALL
-            }
-
-            startForeground(id, notification, types)
+            startForeground(id, notification, FOREGROUND_SERVICE_TYPE_PHONE_CALL)
         } else {
             startForeground(id, notification)
         }
